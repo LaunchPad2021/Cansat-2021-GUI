bc72ef99fb28d15cc0cef4219273a7cb2c46c5fc
fazalfarhan01 <fazal.farhan@gmail.com>
2021-01-28 13:39:08 +0530

    removed redundants

--- assets/demo/demo.js
+++ assets/demo/demo.js
@@ -385,41 +385,41 @@ demo = {
         });
 
 
-        var ctxGreen = document.getElementById("chartLineGreen").getContext("2d");
+        // var ctxGreen = document.getElementById("chartLineGreen").getContext("2d");
 
-        var gradientStroke = ctx.createLinearGradient(0, 230, 0, 50);
+        // var gradientStroke = ctx.createLinearGradient(0, 230, 0, 50);
 
-        gradientStroke.addColorStop(1, 'rgba(66,134,121,0.15)');
-        gradientStroke.addColorStop(0.4, 'rgba(66,134,121,0.0)'); //green colors
-        gradientStroke.addColorStop(0, 'rgba(66,134,121,0)'); //green colors
+        // gradientStroke.addColorStop(1, 'rgba(66,134,121,0.15)');
+        // gradientStroke.addColorStop(0.4, 'rgba(66,134,121,0.0)'); //green colors
+        // gradientStroke.addColorStop(0, 'rgba(66,134,121,0)'); //green colors
 
-        var data = {
-            labels: ['JUL', 'AUG', 'SEP', 'OCT', 'NOV'],
-            datasets: [{
-                label: "My First dataset",
-                fill: true,
-                backgroundColor: gradientStroke,
-                borderColor: '#00d6b4',
-                borderWidth: 2,
-                borderDash: [],
-                borderDashOffset: 0.0,
-                pointBackgroundColor: '#00d6b4',
-                pointBorderColor: 'rgba(255,255,255,0)',
-                pointHoverBackgroundColor: '#00d6b4',
-                pointBorderWidth: 20,
-                pointHoverRadius: 4,
-                pointHoverBorderWidth: 15,
-                pointRadius: 4,
-                data: [90, 27, 60, 12, 80],
-            }]
-        };
+        // var data = {
+        //     labels: ['JUL', 'AUG', 'SEP', 'OCT', 'NOV'],
+        //     datasets: [{
+        //         label: "My First dataset",
+        //         fill: true,
+        //         backgroundColor: gradientStroke,
+        //         borderColor: '#00d6b4',
+        //         borderWidth: 2,
+        //         borderDash: [],
+        //         borderDashOffset: 0.0,
+        //         pointBackgroundColor: '#00d6b4',
+        //         pointBorderColor: 'rgba(255,255,255,0)',
+        //         pointHoverBackgroundColor: '#00d6b4',
+        //         pointBorderWidth: 20,
+        //         pointHoverRadius: 4,
+        //         pointHoverBorderWidth: 15,
+        //         pointRadius: 4,
+        //         data: [90, 27, 60, 12, 80],
+        //     }]
+        // };
 
-        var myChart = new Chart(ctxGreen, {
-            type: 'line',
-            data: data,
-            options: gradientChartOptionsConfigurationWithTooltipGreen
+        // var myChart = new Chart(ctxGreen, {
+        //     type: 'line',
+        //     data: data,
+        //     options: gradientChartOptionsConfigurationWithTooltipGreen
 
-        });
+        // });
 
 
 
@@ -482,37 +482,37 @@ demo = {
         });
 
 
-        var ctx = document.getElementById("CountryChart").getContext("2d");
+        // var ctx = document.getElementById("CountryChart").getContext("2d");
 
-        var gradientStroke = ctx.createLinearGradient(0, 230, 0, 50);
+        // var gradientStroke = ctx.createLinearGradient(0, 230, 0, 50);
 
-        gradientStroke.addColorStop(1, 'rgba(29,140,248,0.2)');
-        gradientStroke.addColorStop(0.4, 'rgba(29,140,248,0.0)');
-        gradientStroke.addColorStop(0, 'rgba(29,140,248,0)'); //blue colors
+        // gradientStroke.addColorStop(1, 'rgba(29,140,248,0.2)');
+        // gradientStroke.addColorStop(0.4, 'rgba(29,140,248,0.0)');
+        // gradientStroke.addColorStop(0, 'rgba(29,140,248,0)'); //blue colors
 
 
-        var myChart = new Chart(ctx, {
-            type: 'bar',
-            responsive: true,
-            legend: {
-                display: false
-            },
-            data: {
-                labels: ['USA', 'GER', 'AUS', 'UK', 'RO', 'BR'],
-                datasets: [{
-                    label: "Countries",
-                    fill: true,
-                    backgroundColor: gradientStroke,
-                    hoverBackgroundColor: gradientStroke,
-                    borderColor: '#1f8ef1',
-                    borderWidth: 2,
-                    borderDash: [],
-                    borderDashOffset: 0.0,
-                    data: [53, 20, 10, 80, 100, 45],
-                }]
-            },
-            options: gradientBarChartConfiguration
-        });
+        // var myChart = new Chart(ctx, {
+        //     type: 'bar',
+        //     responsive: true,
+        //     legend: {
+        //         display: false
+        //     },
+        //     data: {
+        //         labels: ['USA', 'GER', 'AUS', 'UK', 'RO', 'BR'],
+        //         datasets: [{
+        //             label: "Countries",
+        //             fill: true,
+        //             backgroundColor: gradientStroke,
+        //             hoverBackgroundColor: gradientStroke,
+        //             borderColor: '#1f8ef1',
+        //             borderWidth: 2,
+        //             borderDash: [],
+        //             borderDashOffset: 0.0,
+        //             data: [53, 20, 10, 80, 100, 45],
+        //         }]
+        //     },
+        //     options: gradientBarChartConfiguration
+        // });
 
     },
 
